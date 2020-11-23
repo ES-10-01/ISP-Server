b596b9554e1d40e6ba4064e22bbcecbcf422daf3
Dmitry Dobrovitsky <official.dobrosldm@gmail.com>
2020-11-23 15:40:13 +0300

    update schemas

--- src/main/kotlin/com/isp/server/models/Schemas.kt
+++ src/main/kotlin/com/isp/server/models/Schemas.kt
@@ -6,6 +6,18 @@ import org.springframework.data.mongodb.core.mapping.Document
 @Document
 data class User(
         @Id
-        val id:String? = null,
-        val name:String
+        val uid: Int, // ? = null
+        val password: String,
+        val name: String,
+        val surname: String,
+        val privileges: String
+        //val availableLocks: Array<Lock>
+)
+
+@Document
+data class Lock(
+        @Id
+        val uid: Int,
+        val name: String,
+        val ip: String
 )
