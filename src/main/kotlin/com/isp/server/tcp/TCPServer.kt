1722b9ef34200a9f344544f61da95d8786bd9f30
Arsenii <ar.poryadin@yandex.ru>
2020-12-13 01:32:38 +0300

    migrate tcp example config to separate file

--- src/main/kotlin/com/isp/server/tcp/TCPServer.kt
+++ src/main/kotlin/com/isp/server/tcp/TCPServer.kt
@@ -2,6 +2,7 @@ package com.isp.server.tcp
 
 import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.annotation.Transformer
 import org.springframework.integration.channel.DirectChannel
@@ -18,60 +19,62 @@ import org.springframework.integration.transformer.ObjectToStringTransformer
 import org.springframework.messaging.MessageChannel
 import org.springframework.messaging.MessageHandler
 
+@Configuration
+class TCPServer {
+    @Bean
+    fun server(cf: TcpNetServerConnectionFactory?): TcpReceivingChannelAdapter {
+        val adapter = TcpReceivingChannelAdapter()
+        adapter.setConnectionFactory(cf)
+        adapter.setOutputChannel(outputChannel())   //TODO obrati vnimanie sdelano v Germanii
+        return adapter
+    }
 
-@Bean
-fun server(cf: TcpNetServerConnectionFactory?): TcpReceivingChannelAdapter {
-    val adapter = TcpReceivingChannelAdapter()
-    adapter.setConnectionFactory(cf)
-    adapter.setOutputChannel(outputChannel())   //TODO obrati vnimanie sdelano v Germanii
-    return adapter
-}
+    @Bean
+    fun inputChannel(): MessageChannel? {
+        return QueueChannel()
+    }
 
-@Bean
-fun inputChannel(): MessageChannel? {
-    return QueueChannel()
-}
+    @Bean
+    fun outputChannel(): MessageChannel {
+        return DirectChannel()
+    }
 
-@Bean
-fun outputChannel(): MessageChannel {
-    return DirectChannel()
-}
+    @Bean
+    fun cf(): TcpNetServerConnectionFactory {
+        return TcpNetServerConnectionFactory(9876)
+    }
 
-@Bean
-fun cf(): TcpNetServerConnectionFactory {
-    return TcpNetServerConnectionFactory(9876)
-}
-
-@Bean
-fun outbound(): IntegrationFlow? {
-    return IntegrationFlows.from(outputChannel())
+    @Bean
+    fun outbound(): IntegrationFlow? {
+        return IntegrationFlows.from(outputChannel())
             .handle(sender())
             .get()
-}
+    }
 
-@Bean
-fun sender(): MessageHandler {
-    val tcpSendingMessageHandler = TcpSendingMessageHandler()
-    tcpSendingMessageHandler.setConnectionFactory(cf())
-    return tcpSendingMessageHandler
-}
+    @Bean
+    fun sender(): MessageHandler {
+        val tcpSendingMessageHandler = TcpSendingMessageHandler()
+        tcpSendingMessageHandler.setConnectionFactory(cf())
+        return tcpSendingMessageHandler
+    }
 
-@Bean
-fun listener(): ApplicationListener<TcpConnectionOpenEvent> {
-    return ApplicationListener { event ->
-        outputChannel().send(MessageBuilder.withPayload("foo")
+    @Bean
+    fun listener(): ApplicationListener<TcpConnectionOpenEvent> {
+        return ApplicationListener { event ->
+            outputChannel().send(MessageBuilder.withPayload("foo")
                 .setHeader(IpHeaders.CONNECTION_ID, event.getConnectionId())
                 .build())
+        }
     }
-}
 
-@Transformer(inputChannel = "outputChannel", outputChannel = "serviceChannel")
-@Bean
-fun transformer(): ObjectToStringTransformer? {
-    return ObjectToStringTransformer()
-}
+    @Transformer(inputChannel = "outputChannel", outputChannel = "serviceChannel")
+    @Bean
+    fun transformer(): ObjectToStringTransformer? {
+        return ObjectToStringTransformer()
+    }
 
-@ServiceActivator(inputChannel = "serviceChannel")
-fun service(str: String?) {
-    println(str)
+    @ServiceActivator(inputChannel = "serviceChannel")
+    fun service(str: String?) {
+        println(str)
+    }
 }
